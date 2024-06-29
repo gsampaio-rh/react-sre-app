e58d7a28a72a9099722a0f6f8dd68877d2c39d17
Gabriel Sampaio <gsampaio@redhat.com>
2024-06-29 15:33:08 -0300

    improving SLIs

--- src/components/MetricsDetails.js
+++ src/components/MetricsDetails.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUsers, faHourglassHalf, faThumbsUp, faWrench } from '@fortawesome/free-solid-svg-icons';
 
-const MetricsDetails = ({ onMetricClick, selectedMetric }) => {
+const MetricsDetails = () => {
+    const [selectedMetric, setSelectedMetric] = useState(null);
+
     const metrics = [
-        { id: 'tempoResposta', value: '40', label: 'Tempo de Resposta (segundos)', icon: faHourglassHalf, sli: 99.99, description: '99.99% da resposta abaixo de 1 minuto.' },
-        { id: 'quantidadePessoas', value: '200', label: 'Quantidade de Pessoas por Dia', icon: faUsers, sli: 95, description: '95% das pessoas atendidas.' },
-        { id: 'satisfacaoUsuarios', value: '10', label: 'Satisfação dos Usuários', icon: faThumbsUp, sli: 40, description: '40% dos usuários satisfeitos.' },
-        { id: 'manutencoesRealizadas', value: '5', label: 'Manutenções Realizadas', icon: faWrench, sli: 85, description: '85% das manutenções realizadas com sucesso' }
+        { id: 'tempoResposta', value: '40', label: 'Tempo de Resposta (segundos)', icon: faHourglassHalf, sli: 99.99, description: 'da resposta abaixo de 1 minuto.' },
+        { id: 'quantidadePessoas', value: '200', label: 'Quantidade de Pessoas por Dia', icon: faUsers, sli: 95, description: 'das pessoas atendidas.' },
+        { id: 'satisfacaoUsuarios', value: '10', label: 'Satisfação dos Usuários', icon: faThumbsUp, sli: 90, description: 'dos usuários satisfeitos.' },
+        { id: 'manutencoesRealizadas', value: '5', label: 'Manutenções Realizadas', icon: faWrench, sli: 85, description: 'das manutenções realizadas com sucesso.' }
     ];
 
-    return(
+    const handleMetricClick = (id) => {
+        setSelectedMetric(selectedMetric === id ? null : id);
+    };
+
+    return (
         <div className="metrics-details">
             {metrics.map(metric => (
-                <div
-                    key={metric.id}
-                    className={`metric ${selectedMetric === metric.id ? 'selected' : ''}`}
-                    onClick={() => onMetricClick(metric.id)}
-                >
-                    <FontAwesomeIcon icon={metric.icon} className="icon" />
-                    <p className="metric-value">{metric.value}</p>
-                    <p className="metric-label">{metric.label}</p>
-                    {selectedMetric === metric.id && (
-                        <div className={`metric-sli ${metric.sli >= 95 ? 'positive' : 'negative'}`}>
-                            <strong>SLI:</strong> {metric.description}
+                <div key={metric.id} className={`metric ${selectedMetric === metric.id ? 'flipped' : ''}`} onClick={() => handleMetricClick(metric.id)}>
+                    <div className="metric-inner">
+                        <div className="metric-front">
+                            <FontAwesomeIcon icon={metric.icon} className="icon" />
+                            <p className="metric-value">{metric.value}</p>
+                            <p className="metric-label">{metric.label}</p>
                         </div>
-                    )}
+                        <div className={`metric-back ${metric.sli >= 95 ? 'positive' : 'negative'}`}>
+                            <div className="metric-sli">
+                                <strong>SLI:</strong> {metric.sli}% {metric.description}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             ))}
         </div>
