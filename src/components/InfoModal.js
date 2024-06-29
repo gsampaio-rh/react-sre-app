53dc0a1f87e6a66c0dc6e94fcf3285651d75a448
Gabriel Sampaio <gsampaio@redhat.com>
2024-06-29 16:44:46 -0300

    stable sli and slo

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -8,6 +8,7 @@ const InfoModal = ({ show, handleClose, componentData }) => {
     const { isInsightsEnabled } = useInsightsContext();
     const [showMetrics, setShowMetrics] = useState(false);
     const [selectedMetric, setSelectedMetric] = useState(null);
+    const [hoveredMetric, setHoveredMetric] = useState(null);
 
     if (!componentData) return null;
 
@@ -19,6 +20,14 @@ const InfoModal = ({ show, handleClose, componentData }) => {
 
     const handleMetricClick = (metricId) => {
         setSelectedMetric(selectedMetric === metricId ? null : metricId);
+    };
+
+    const handleChartIconHover = (metricId) => {
+        setHoveredMetric(metricId);
+    };
+
+    const handleChartIconLeave = () => {
+        setHoveredMetric(null);
     };
 
     return (
@@ -33,15 +42,32 @@ const InfoModal = ({ show, handleClose, componentData }) => {
                         alt="Lamp Icon"
                     />
                 </div>
-                <ComponentDetails componentData={componentData} />
+                {!hoveredMetric && <ComponentDetails componentData={componentData} />}
+                {hoveredMetric && (
+                    <div className="chart-container">
+                        <img src={`/img/${hoveredMetric}.png`} alt="Chart" />
+                    </div>
+                )}
                 {showMetrics && (
                     <MetricsDetails
                         onMetricClick={handleMetricClick}
                         selectedMetric={selectedMetric}
+                        onChartIconHover={handleChartIconHover}
+                        onChartIconLeave={handleChartIconLeave}
                     />
                 )}
             </Modal.Body>
             <Modal.Footer>
+                {isInsightsEnabled && (
+                    <a
+                        href="https://www.redhat.com/en/technologies/management/insights"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}
+                    >
+                        <img src="/img/redhat-insights.png" className="logo" alt="Red Hat Insights Logo" style={{ width: '150px', marginRight: '10px' }} />
+                    </a>
+                )}
                 <Button variant="secondary" onClick={handleClose}>
                     Fechar
                 </Button>
--- src/components/MetricsDetails.js
+++ src/components/MetricsDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUsers, faHourglassHalf, faThumbsUp, faWrench, faChartLine } from '@fortawesome/free-solid-svg-icons';
 import metricsData from '../assets/data/sli_slo.json';
@@ -11,23 +11,15 @@ const iconMapping = {
     faWrench: faWrench
 };
 
-const MetricsDetails = () => {
-    const [flippedMetrics, setFlippedMetrics] = useState({});
-
-    const handleMetricClick = (id) => {
-        setFlippedMetrics(prevState => ({
-            ...prevState,
-            [id]: !prevState[id]
-        }));
-    };
+const MetricsDetails = ({ onMetricClick, selectedMetric, onChartIconHover, onChartIconLeave }) => {
 
     return (
         <div className="metrics-details">
             {metricsData.map(metric => (
                 <div
                     key={metric.id}
-                    className={`metric ${flippedMetrics[metric.id] ? 'flipped' : ''}`}
-                    onClick={() => handleMetricClick(metric.id)}
+                    className={`metric ${selectedMetric === metric.id ? 'flipped' : ''}`}
+                    onClick={() => onMetricClick(metric.id)}
                 >
                     <div className="metric-inner">
                         <div className="metric-front">
@@ -36,15 +28,22 @@ const MetricsDetails = () => {
                             <p className="metric-label">{metric.label}</p>
                         </div>
                         <div className={`metric-back ${metric.sli >= metric.slo ? 'positive' : 'negative'}`}>
-                            <div className="chart-icon-container">
+                            <div
+                                className="chart-icon-container"
+                                onMouseEnter={() => onChartIconHover(metric.id)}
+                                onMouseLeave={onChartIconLeave}
+                            >
                                 <FontAwesomeIcon icon={faChartLine} className="chart-icon" />
-                                <div className="chart-tooltip">
-                                    <img src={`/img/${metric.id}.png`} alt="Chart" />
-                                </div>
                             </div>
-                            <div className="metric-sli">SLI:</div>
-                            <div className="metric-percentage">{metric.sli}%</div>
-                            <div className="metric-description">{metric.description}</div>
+                            <div className="metric-sli">
+                                SLI:
+                            </div>
+                            <div className="metric-percentage">
+                                {metric.sli}%
+                            </div>
+                            <div className="metric-description">
+                                {metric.description}
+                            </div>
                         </div>
                     </div>
                 </div>
