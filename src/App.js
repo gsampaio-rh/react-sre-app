0872c730ba08ca5785ea6ed1b059fad6eecc2080
Gabriel Sampaio <you@example.com>
2024-06-29 08:40:01 -0300

    affected equipment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './assets/css/styles.css';  // Import the styles
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Floor from './components/Floor';
 import ComponentModal from './components/ComponentModal';
-import './assets/css/styles.css';  // Import the styles
 import infraData from './assets/data/infra_data.json';
+import cveData from './assets/data/active_cve_data.json';
 
 function App() {
   const [showModal, setShowModal] = useState(false);
   const [selectedComponent, setSelectedComponent] = useState(null);
   const [selectedProblem, setSelectedProblem] = useState(null);
+
+  useEffect(() => {
+    const affectedEquipment = [];
+    const matchingCVEs = [];
+
+    for (const componentId in infraData.equipment) {
+      const component = infraData.equipment[componentId];
+      const relatedProblems = component.related_problems;
+
+      relatedProblems.forEach(problemId => {
+        const problem = infraData.problems[problemId];
+
+        const matchingCVE = cveData.data.find(cve =>
+          cve.attributes.rules.some(rule => rule.associated_cves.includes(problemId))
+        );
+
+        if (matchingCVE) {
+          affectedEquipment.push(component.name);
+          matchingCVEs.push(matchingCVE.attributes.synopsis);
+        }
+      });
+    }
+
+    console.log('Affected Equipment:', affectedEquipment);
+    console.log('Matching CVEs:', matchingCVEs);
+  }, []);
 
   const handleClose = () => setShowModal(false);
 
@@ -21,7 +48,23 @@ function App() {
     }
 
     const relatedProblems = componentData.related_problems;
-    const problemData = relatedProblems.length > 0 ? infraData.problems[relatedProblems[0]] : null;
+    let problemData = null;
+
+    if (relatedProblems.length > 0) {
+      const problemId = relatedProblems[0];
+      problemData = infraData.problems[problemId];
+
+      const matchingCVE = cveData.data.find(cve =>
+        cve.attributes.rules.some(rule => rule.associated_cves.includes(problemId))
+      );
+
+      if (matchingCVE) {
+        problemData = {
+          ...problemData,
+          cveDetails: matchingCVE.attributes
+        };
+      }
+    }
 
     setSelectedComponent(componentData);
     setSelectedProblem(problemData);
@@ -30,6 +73,7 @@ function App() {
 
   // Print the data when the page loads
   console.log(infraData);
+  console.log(cveData);
 
   return (
     <div className="App">
